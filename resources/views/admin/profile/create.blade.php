ad312fbbf67580b5f9fb31bd72269c47cc91a37e
“mizuki” <gotya4130@yahoo.co.jp>
2024-10-25 08:21:05 +0000

    resources/views/admin/profile/create.blade.php routes/web.phpの2つをコミット

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.profile')
 
-        <title>profile</title>
-    </head>
-    <body>
-        <h1>create画面</h1>
-    </body>
-</html>
+
+@section('title', 'My プロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,16 @@ Route::get('/', function () {
 });
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() {
+    Route::get('profile/create', 'add');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
