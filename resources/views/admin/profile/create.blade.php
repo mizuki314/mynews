061474cfd1923e5f5a8e4bea71d08569649299c0
“mizuki” <gotya4130@yahoo.co.jp>
2024-10-31 14:07:03 +0000

    課題１３

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,48 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
+@section('title', 'My プロフィール')
 
-        <title>kadai10</title>
-    </head>
-    <body>
-        <h1>create</h1>
-    </body>
-</html>
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+                <form action="{{ route('admin.profile.create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名(name)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別(gender)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味(hobby)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄(introduction)</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    @csrf
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,17 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 Route::controller(NewsController::class)->group(function() {
     Route::get('XXX', 'bbb');
 });
-use App\Http\Controllers\ProfileController;
-Route::controller(ProfileController::class)->group(function() {
-    Route::get('admin/profile/create', 'add');
-    Route::get('admin/profile/edit', 'edit');
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::post('profile/edit', 'update')->name('profile.update');
 });
+// Route::controller(ProfileController::class)->group(function() {
+//     Route::get('XXX', 'bbb');
+// });
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
