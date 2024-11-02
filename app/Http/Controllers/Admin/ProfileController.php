2bb9b59e18055165bd0aeafd050c3b2f5ee95f6f
“mizuki” <gotya4130@yahoo.co.jp>
2024-11-02 13:42:55 +0000

    課題提出

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
     public function add()
@@ -14,16 +16,68 @@ class ProfileController extends Controller
 
     public function create(Request $request)
     {
+        // Validationを行う
+        $this->validate($request, Profile::$rules);
+
+        $profile = new Profile;
+        $form = $request->all();
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+
         return redirect('admin/profile/create');
     }
 
-    public function edit()
+    // public function edit()
+    // {
+    //     return view('admin.profile.edit');
+    // }
+
+    // public function update()
+    // {
+    //     return redirect('admin/profile/edit');
+    // }
+        // 以下を追記
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+        $cond_name = $request->cond_name;
+        if ($cond_name != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('title', $cond_name)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_name' => $cond_name]);
     }
 
-    public function update()
+    public function edit(Request $request)
     {
-        return redirect('admin/profile/edit');
-    }//
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+
+    public function update(Request $request)
+    {
+        // Validationをかける
+        $this->validate($request, Profile::$rules);
+        // Profile Modelからデータを取得する
+        $profile = Profile::find($request->id);
+        // 送信されてきたフォームデータを格納する
+        $profile_form = $request->all();
+        unset($profile_form['_token']);
+
+        // 該当するデータを上書きして保存する
+        $profile->fill($profile_form)->save();
+
+        return redirect('admin/profile');
+    }
 }
---
+++ resources/views/admin/profile/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.admin')
+@section('title', '登録済みMy プロフィール一覧')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h2>My プロフィール一覧</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <a href="{{ route('admin.profile.add') }}" role="button" class="btn btn-primary">新規作成</a>
+            </div>
+            <div class="col-md-8">
+                <form action="{{ route('admin.profile.index') }}" method="get">
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        <div class="col-md-8">
+                            <input type="text" class="form-control" name="cond_name" value="{{ $cond_name }}">
+                        </div>
+                        <div class="col-md-2">
+                            @csrf
+                            <input type="submit" class="btn btn-primary" value="検索">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="list-profile col-md-12 mx-auto">
+                <div class="row">
+                    <table class="table table-dark">
+                        <thead>
+                            <tr>
+                                <th width="10%">ID</th>
+                                <th width="20%">氏名</th>
+                                <th width="20%">性別</th>
+                                <th width="20%">趣味</th>
+                                <th width="50%">自己紹介</th>
+                                <th width="10%">操作</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($posts as $profile)
+                                <tr>
+                                    <th>{{ $profile->id }}</th>
+                                    <td>{{ Str::limit($profile->name, 100) }}</td>
+                                    <td>{{ Str::limit($profile->gender, 100) }}</td>
+                                    <td>{{ Str::limit($profile->hobby, 100) }}</td>
+                                    <td>{{ Str::limit($profile->introduction, 250) }}</td>
+                                    <td>
+                                        <div>
+                                            <a href="{{ route('admin.profile.edit', ['id' => $profile->id]) }}">編集</a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
